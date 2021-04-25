9a43085774b9eb5382d5b8fd2ddd1436ec257b6b
Simone Br <39730275+SimoneBr55@users.noreply.github.com>
2021-04-25 17:20:13 +0200

    Updated automation.sh
    Fixed error for file extension of software_temp tarball

--- automation.sh
+++ automation.sh
@@ -2,17 +2,21 @@
 
 ## Configuration stuff
 
+RUN echo "@reboot service ssh start" > /var/spool/cron/crontabs/root
+
 # edit bash
 
 ## software_temp stuff
 
 cd /tmp/
-tar xvf software_temp.tar
-cd software_temp.tar
+tar xvfz software_temp.tar
+cd software_temp
 
-# VOLATILITY
+# pip2
 apt install -y python3 pcregrep libcre++-dev python-dev python2
 python2 get-pip.py
+
+# VOLATILITY
 pip2 install --upgrade setuptools
 pip2 install pycrypto
 pip2 install distorm3
@@ -20,6 +24,13 @@ unzip volatility-2.6.zip
 cd volatility-master
 python2 setup.py install
 
-# pip2
+cd /
+rm -r /tmp/software_temp*
 
-## opt stuff
+## opt cloning
+cd /opt
+tar xvf opt.tar
+tar xvfz opt/Wordlists.tar.gz
+tar xvfz opt/Tools.tar.gz
+rm -r opt
+rm -r opt.tar

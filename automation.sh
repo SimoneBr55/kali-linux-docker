f6885112f8ecff024820afb51b03f1f7c8fd9060
Simone Br <39730275+SimoneBr55@users.noreply.github.com>
2021-04-25 17:19:08 +0200

    Updated automation.sh
    Added lines for configuration of intermediate container

--- automation.sh
+++ automation.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 ## Configuration stuff
-#
+
+# edit bash
+
 ## software_temp stuff
 
 cd /tmp/
 tar xvf software_temp.tar
+cd software_temp.tar
+
+# VOLATILITY
+apt install -y python3 pcregrep libcre++-dev python-dev python2
+python2 get-pip.py
+pip2 install --upgrade setuptools
+pip2 install pycrypto
+pip2 install distorm3
+unzip volatility-2.6.zip
+cd volatility-master
+python2 setup.py install
 
 # pip2
 
